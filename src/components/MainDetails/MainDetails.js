e48f05bd2e4e8626a46b5aee9973255751ed9dd0
Salim Hamed <salimhamed@gmail.com>
2020-01-14 21:55:17 -0800

    Remove lower sections

--- src/components/MainDetails/MainDetails.js
+++ src/components/MainDetails/MainDetails.js
@@ -14,7 +14,7 @@ function MainDetails() {
                     <Col className={styles.details}>
                         <h1 className={styles.announcement}>Salim and Lledó are getting married!</h1>
                         <h1 className={styles.date}>August 22, 2020</h1>
-                        <h4 className={styles.location}>L'ampolla, Catalonia</h4>
+                        <h4 className={styles.location}>L'Ampolla, Catalonia</h4>
                         <h4 className={styles.hashtag}>#lledoisalim</h4>
                     </Col>
                 </Row>
