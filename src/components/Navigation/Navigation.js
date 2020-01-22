f64378683e5ad96711d04a25ce920fbdf0894b1f
Salim Hamed <salimhamed@gmail.com>
2020-01-21 22:38:03 -0800

    Add a language selector

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from "react-bootstrap/Navbar"
 import Nav from "react-bootstrap/Nav"
+import NavDropdown from "react-bootstrap/NavDropdown"
 
 import styles from "./Navigation.module.scss"
 
@@ -8,10 +9,16 @@ function Navigation() {
     return (
         <Navbar variant="dark" expand="lg" sticky="top" className={styles.navigation}>
             <Navbar.Brand href="#home" className={styles.brand}>S & L</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-            <Navbar.Collapse id="basic-navbar-nav">
+            <Navbar.Toggle/>
+            <Navbar.Collapse>
                 <Nav>
                     <Nav.Link href="#home">Home</Nav.Link>
+                </Nav>
+                <Nav className="ml-auto">
+                    <NavDropdown title="English">
+                        <NavDropdown.Item eventKey="english">English</NavDropdown.Item>
+                        <NavDropdown.Item eventKey="catalan">Catalan</NavDropdown.Item>
+                    </NavDropdown>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
