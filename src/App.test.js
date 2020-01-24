b45b16c3caa65c83fb8ea5d38b3c8a3b1f9057a7
Salim Hamed <salimhamed@gmail.com>
2020-01-23 19:19:31 -0800

    Update tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
 import React from "react"
 import { render } from "@testing-library/react"
+
 import App from "./App"
 
 test("renders learn react link", () => {
     const { getByText } = render(<App />)
-    const linkElement = getByText(/learn react/i)
+    const linkElement = getByText(/Home/i)
     expect(linkElement).toBeInTheDocument()
 })
