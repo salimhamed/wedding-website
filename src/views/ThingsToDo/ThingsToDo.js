24991eecf4e938b2130467c474d69da53337088d
Salim Hamed <salimhamed@gmail.com>
2020-01-26 18:57:32 -0800

    Add things to do

--- src/views/ThingsToDo/ThingsToDo.js
+++ src/views/ThingsToDo/ThingsToDo.js
@@ -1,7 +1,11 @@
 import React, { useContext } from "react"
+import Container from "react-bootstrap/Container"
 
 import { Store } from "store"
-import { title } from "content/ThingsToDo"
+import { title, thingsToDo } from "content/ThingsToDo"
+import { Header } from "components/Header"
+import { Item } from "components/Item"
+import headerImg from "photos/sky_diving.jpeg"
 
 function ThingsToDo() {
     const { state } = useContext(Store)
@@ -10,15 +14,35 @@ function ThingsToDo() {
     } = state
 
     const { Heading, SubHeading } = title[language]
+    const thingsToDoItems = thingsToDo[language]
 
     return (
         <>
-            <h1>
-                <Heading />
-            </h1>
-            <h2>
-                <SubHeading />
-            </h2>
+            <Header
+                imageUrl={headerImg}
+                Heading={Heading}
+                SubHeading={SubHeading}
+            />
+            <Container fluid>
+                {thingsToDoItems.map(
+                    ({ Title, Address, Details, link }, idx) => (
+                        <Item
+                            key={idx}
+                            LeftMainTitle={Title}
+                            RightFirstContact={() => (
+                                <a
+                                    href={link}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    <Address />
+                                </a>
+                            )}
+                            RightDetails={Details}
+                        />
+                    )
+                )}
+            </Container>
         </>
     )
 }
