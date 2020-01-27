fd1b128b19f47a6620a0240157c315b5c4b7ac6b
Salim Hamed <salimhamed@gmail.com>
2020-01-26 19:15:57 -0800

    create footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+import Container from "react-bootstrap/Container"
+import Col from "react-bootstrap/Col"
+
+
+const Footer = () => (
+    <Container fluid>
+        <Col md={12}>
+
+        </Col>
+    </Container>
+)
+
+export default Footer
