4c55f65d003e9ac266937328f179e72ac36000bd
Lledo <lledoe@gmail.com>
2020-02-06 18:18:50 -0800

    catalan: Travel/Title

--- src/content/Schedule/Title.js
+++ src/content/Schedule/Title.js
@@ -7,9 +7,7 @@ export default {
         Heading: () => <>Schedule</>,
         SubHeading: () => (
             <span>
-                Here's what to expect during our wedding weekend. There will
-                also be a printout of this schedule available in your hotel
-                rooms. We can't wait to celebrate with you!
+                Here's what to expect during our wedding weekend. We can't wait to celebrate with you!
             </span>
         ),
     },
@@ -17,9 +15,8 @@ export default {
         Heading: () => <>El Plan</>,
         SubHeading: () => (
             <span>
-                Here's what to expect during our wedding weekend. There will
-                also be a printout of this schedule available in your hotel
-                rooms. We can't wait to celebrate with you!
+                Això és el què tenim planejat durant el nostre cap de setmana de noces. Quines ganes tenim de
+                celebrar-ho amb vosaltres!
             </span>
         ),
     },
