14fc87f8649a171a47079d75c159b1b22555b629
Lledo <lledoe@gmail.com>
2020-02-07 17:14:29 -0800

    things to do

--- src/content/ThingsToDo/ThingsToDo.js
+++ src/content/ThingsToDo/ThingsToDo.js
@@ -1,190 +1,280 @@
 import React from "react"
 
-import { LANGUAGE } from "actions/constants"
+import {LANGUAGE} from "actions/constants"
 
 export default {
     [LANGUAGE.EN]: [
         {
             Title: () => <>Delta de l'Ebre</>,
-            Address: () => (
-                <>
-                    <p>Golden Gate Bridge</p>
-                    <p>San Francisco, CA</p>
-                </>
-            ),
             Details: () => (
                 <>
                     <p>
-                        We know this is cliché, but it’s a must see. A great
-                        walk or bike ride for the rare day that it is sunny out.
+                        The Natural Park of the Ebro Delta offers visitors a wide range of itineraries to discover
+                        the most interesting places in the Delta, on foot, by bicycle, canoe, boat or horseback. You can
+                        discover its birdwatching, mussel farms, kitesurfing, sailing and amazing beaches.
                     </p>
-                    <p>More text.</p>
                 </>
             ),
-            link:
-                "https://www.google.com/maps/place/Golden+Gate+Bridge,+California/@37.8201012,-122.4807313,17z/data=!3m1!4b1!4m5!3m4!1s0x80858690719d9b63:0x4657f618df61a5c3!8m2!3d37.8201012!4d-122.4785426",
+            link: "https://www.deltaturistic.com/en/the-ebro-delta/tourism-the-ebro/"
         },
         {
             Title: () => <>Parc Natural dels Ports</>,
-            Address: () => (
-                <>
-                    <p>Golden Gate Bridge</p>
-                    <p>San Francisco, CA</p>
-                </>
-            ),
             Details: () => (
                 <>
                     <p>
-                        We know this is cliché, but it’s a must see. A great
-                        walk or bike ride for the rare day that it is sunny out.
+                        The Els Ports Natural Park is located between the regions of Baix Ebre, Montsià and Terra Alta.
+                        It is distinguished by its shapes, its two differing climates, as well as its fauna,
+                        a highlight being the Spanish ibex, to which the park acts as a reserve. Don't miss the
+                        canyoning!
                     </p>
-                    <p>More text.</p>
                 </>
             ),
-            link:
-                "https://www.google.com/maps/place/Golden+Gate+Bridge,+California/@37.8201012,-122.4807313,17z/data=!3m1!4b1!4m5!3m4!1s0x80858690719d9b63:0x4657f618df61a5c3!8m2!3d37.8201012!4d-122.4785426",
+            link: "http://www.catalunya.com/els-ports-natural-park-17-17001-573555?language=en",
         },
         {
             Title: () => <>Tortosa</>,
-            Address: () => (
-                <>
-                    <p>Golden Gate Bridge</p>
-                    <p>San Francisco, CA</p>
-                </>
-            ),
             Details: () => (
                 <>
                     <p>
-                        We know this is cliché, but it’s a must see. A great
-                        walk or bike ride for the rare day that it is sunny out.
+                        Few cities without the status of provincial capital are as important as Tortosa, which boasts
+                        2000 years of history. Every corner of the beautiful old town is steeped in history.
+                        The Iberians chose to settle in what is now the capital of the Baix Ebre region. In fact,
+                        historians believe that it may had been the enigmatic and important city of Hibera.
+                        If that was indeed the case, the Ebro (Iber) river and the entire peninsula were named after
+                        the city. As proof of its splendid past, which saw Romans, Muslims, Jews and Christians pass
+                        through, Tortosa boasts the impressive Suda Castle, Saint Mary’s Cathedral and some elegant
+                        Modernist buildings. It isn’t known as the city of three cultures (Jewish, Arab and Christian)
+                        for nothing. Tortosa experienced its golden age during the Renaissance (16th century).
                     </p>
-                    <p>More text.</p>
                 </>
             ),
-            link:
-                "https://www.google.com/maps/place/Golden+Gate+Bridge,+California/@37.8201012,-122.4807313,17z/data=!3m1!4b1!4m5!3m4!1s0x80858690719d9b63:0x4657f618df61a5c3!8m2!3d37.8201012!4d-122.4785426",
+            link: "https://www.tortosaturisme.cat/en/",
         },
         {
-            Title: () => <>L'Ampolla</>,
-            Address: () => (
-                <>
-                    <p>Golden Gate Bridge</p>
-                    <p>San Francisco, CA</p>
-                </>
-            ),
+            Title: () => <>L'Ampolla and L'Ametlla de Mar</>,
             Details: () => (
                 <>
                     <p>
-                        We know this is cliché, but it’s a must see. A great
-                        walk or bike ride for the rare day that it is sunny out.
+                        Can't miss these genuine fishing villages; enjoy their beaches (platja Santa Llúcia, Morro de
+                        Gos, Cap Roig, and Estany Tort), rent a boat an tour around the Delta to Sant Carles de la
+                        Ràpita (where you can visit the mussel farms), swim among tunas, and eat all the food!!
                     </p>
-                    <p>More text.</p>
                 </>
             ),
-            link:
-                "https://www.google.com/maps/place/Golden+Gate+Bridge,+California/@37.8201012,-122.4807313,17z/data=!3m1!4b1!4m5!3m4!1s0x80858690719d9b63:0x4657f618df61a5c3!8m2!3d37.8201012!4d-122.4785426",
-        },
-        {
-            Title: () => <>Centre Picasso</>,
-            Address: () => (
-                <>
-                    <p>Golden Gate Bridge</p>
-                    <p>San Francisco, CA</p>
-                </>
-            ),
-            Details: () => (
-                <>
-                    <p>
-                        We know this is cliché, but it’s a must see. A great
-                        walk or bike ride for the rare day that it is sunny out.
-                    </p>
-                    <p>More text.</p>
-                </>
-            ),
-            link:
-                "https://www.google.com/maps/place/Golden+Gate+Bridge,+California/@37.8201012,-122.4807313,17z/data=!3m1!4b1!4m5!3m4!1s0x80858690719d9b63:0x4657f618df61a5c3!8m2!3d37.8201012!4d-122.4785426",
+            link: "https://ampollaturisme.com/en/",
         },
         {
             Title: () => <>Castell de Miravet</>,
             Address: () => (
                 <>
-                    <p>Golden Gate Bridge</p>
-                    <p>San Francisco, CA</p>
+                    <p>Camí del Castell, s/n</p>
+                    <p>43747 Miravet</p>
                 </>
             ),
             Details: () => (
                 <>
                     <p>
-                        We know this is cliché, but it’s a must see. A great
-                        walk or bike ride for the rare day that it is sunny out.
+                        Twelfth century Templar fortress, built on a hill that controls throughout the Ebro river that
+                        passes by his feet and dominates the town of Miravet. In good condition, is one of the great
+                        examples of its kind in Western Europe. You can visit different rooms as the walls and stables,
+                        cistern, kitchen, refectory, warehouses or important Romanesque church. It is managed by the
+                        Catalan Cultural Heritage of the Generalitat of Catalonia.
                     </p>
-                    <p>More text.</p>
                 </>
             ),
-            link:
-                "https://www.google.com/maps/place/Golden+Gate+Bridge,+California/@37.8201012,-122.4807313,17z/data=!3m1!4b1!4m5!3m4!1s0x80858690719d9b63:0x4657f618df61a5c3!8m2!3d37.8201012!4d-122.4785426",
+            link: "http://www.turismemiravet.cat/proposta/el-castell-de-miravet/",
+        },
+        {
+            Title: () => <>Centre Picasso</>,
+            Address: () => (
+                <>
+                    <p>Carrer de l'Hospital, s/n</p>
+                    <p>43596 Horta de Sant Joan</p>
+                </>
+            ),
+            Details: () => (
+                <>
+                    <p>
+                        In order to preserve and promote the legacy that the painter from Malaga left in this
+                        municipality, the Picasso Center exhibits facsimile reproductions of almost 200 works done
+                        during the two stays the artist made in this village and also other paintings painted in
+                        Barcelona or in Paris, but inspired by the landscape of Horta. The Picasso Center is located in
+                        the old hospital, a Renaissance building that was built to welcome patients and passers-by.
+                    </p>
+                </>
+            ),
+            link: "https://terresdelebre.travel/en/activities-what-to-do/culture/museums-and-visitor-centres/the-picasso-centre-horta-de-sant-joan",
         },
         {
             Title: () => <>Coves Meravelles</>,
-            Address: () => (
-                <>
-                    <p>Golden Gate Bridge</p>
-                    <p>San Francisco, CA</p>
-                </>
-            ),
             Details: () => (
                 <>
                     <p>
-                        We know this is cliché, but it’s a must see. A great
-                        walk or bike ride for the rare day that it is sunny out.
+                        Although its existence was already known by the people of the town, it was not until 1968 when
+                        the Gracia Center of Speleologists explored them for the first time. The “Meravelles” are the
+                        most impressive of the set of caves called “La Aumidiella”, with a wealth of formations, showing
+                        stalactites, stalagmites, flags, gours, and even the so-called “eccentrics”, which dare to play
+                        with the law of gravity by growing inexplicably in all possible directions.
                     </p>
-                    <p>More text.</p>
                 </>
             ),
-            link:
-                "https://www.google.com/maps/place/Golden+Gate+Bridge,+California/@37.8201012,-122.4807313,17z/data=!3m1!4b1!4m5!3m4!1s0x80858690719d9b63:0x4657f618df61a5c3!8m2!3d37.8201012!4d-122.4785426",
+            link: "https://terresdelebre.travel/en/activities-what-to-do/green-spaces/other-natural-spaces/coves-meravelles",
         },
         {
             Title: () => <>The wine cathedral</>,
             Address: () => (
                 <>
-                    <p>Golden Gate Bridge</p>
-                    <p>San Francisco, CA</p>
+                    <p>Carrer del Pilonet, 8</p>
+                    <p>43594 El Pinell de Brai</p>
                 </>
             ),
             Details: () => (
                 <>
                     <p>
-                        We know this is cliché, but it’s a must see. A great
-                        walk or bike ride for the rare day that it is sunny out.
+                        The Pinell de Brai Catedral del Vi is the cooperative building of the Modernist style of the
+                        early twentieth century designed by Cèsar Martinell, a direct disciple of Antoní Gaudí.
                     </p>
-                    <p>More text.</p>
                 </>
             ),
-            link:
-                "https://www.google.com/maps/place/Golden+Gate+Bridge,+California/@37.8201012,-122.4807313,17z/data=!3m1!4b1!4m5!3m4!1s0x80858690719d9b63:0x4657f618df61a5c3!8m2!3d37.8201012!4d-122.4785426",
+            link: "https://catedraldelvi.com/en/?v=907eba32d950",
         },
     ],
     [LANGUAGE.CAT]: [
         {
-            Title: () => <>Walk the Golden Gate Bridge</>,
+            Title: () => <>Delta de l'Ebre</>,
+            Details: () => (
+                <>
+                    <p>
+                        El Parc Natural del Delta de l’Ebre ofereix als seus visitants una àmplia i variada gamma
+                        d’itineraris per descobrir els indrets més interessants del Delta, sigui a peu, en bici, canoa o
+                        a cavall. Podeu observar les aus, anar a les muscleres, fer kitesurf, navegar i disfrutar de les
+                        platges.
+                    </p>
+                </>
+            ),
+            link: "https://www.deltaturistic.com/el-delta-de-lebre/turisme-per-lebre/",
+        },
+        {
+            Title: () => <>Parc Natural dels Ports</>,
+            Details: () => (
+                <>
+                    <p>
+                        El Parc Natural dels Ports està situat entre les comarques del Baix Ebre, el Montsià i la Terra
+                        Alta. En són característiques les seves formes abruptes i els seus dos climes clarament
+                        diferenciats, amb una rica biodiversitat. No us pergueu el barranquisme! </p>
+                </>
+            ),
+            link: "http://www.catalunya.com/parc-natural-dels-ports-17-17001-573555?language=ca",
+        },
+        {
+            Title: () => <>Tortosa</>,
+            Details: () => (
+                <>
+                    <p>
+                        Hi ha poques ciutats petites que no siguen capital de província amb la transcendència de
+                        Tortosa, amb una història de 2.000 anys. A cada racó del seu riquíssim nucli antic es respiren
+                        èpoques passades. Els ibers van escollir la que avui és la capital de la comarca del Baix Ebre
+                        per establir-se. De fet, els historiadors es plantegen si va arribar a ser l’enigmàtica i
+                        important ciutat Hibera. Si fos així, Tortosa hauria donat nom al riu Ebre (Iber) i a tota la
+                        península. Com a proves del seu esplendorós passat, per on van passar romans, musulmans, jueus i
+                        cristians, Tortosa llueix l’impressionant castell de la Suda, la catedral de Santa Maria i
+                        elegants edificis modernistes. No en va se l’anomena la ciutat de les tres cultures: cristiana,
+                        jueva i àrab. Durant l’època del Renaixement (s. XVI) Tortosa va viure el seu moment de màxima
+                        esplendor.
+                    </p>
+                </>
+            ),
+            link: "https://www.tortosaturisme.cat/",
+        },
+        {
+            Title: () => <>L'Ampolla and L'Ametlla de Mar</>,
+            Details: () => (
+                <>
+                    <p>
+                        No us podeu perdre aquest dos pobles pesquers; les seves platges (platja Santa Llúcia, Morro de
+                        Gos, Cap Roig i Estany Tort), llogueu una barca per costejar el delta fins a Sant Carles de la
+                        Ràpita (on podeu visitar les muscleres), nadeu entre les tonyines i disfruteu del menjar!!
+                    </p>
+                </>
+            ),
+            link: "https://ampollaturisme.com/",
+        },
+        {
+            Title: () => <>Castell de Miravet</>,
             Address: () => (
                 <>
-                    <p>Golden Gate Bridge</p>
-                    <p>San Francisco, CA</p>
+                    <p>Camí del Castell, s/n</p>
+                    <p>43747 Miravet</p>
                 </>
             ),
             Details: () => (
                 <>
                     <p>
-                        We know this is cliché, but it’s a must see. A great
-                        walk or bike ride for the rare day that it is sunny out.
+                        Fortalesa templera del segle XII, construida sobre un turó que controla tot el territori, el riu
+                        Ebre que passa pels seus peus i domina tota la població de Miravet. En bon estat de conservació,
+                        és un dels grans exemples del seu estil a l’Europa occidental. Es poden visitar les muralles i
+                        diferents estances com cavallerisses, cisterna, cuina, refetor, magatzems o l’important església
+                        romànica. Està gestionat per l’Agència Catalana del Patrimoni Cultural de la Generalitat de
+                        Catalunya.
                     </p>
-                    <p>More text.</p>
                 </>
             ),
-            link:
-                "https://www.google.com/maps/place/Golden+Gate+Bridge,+California/@37.8201012,-122.4807313,17z/data=!3m1!4b1!4m5!3m4!1s0x80858690719d9b63:0x4657f618df61a5c3!8m2!3d37.8201012!4d-122.4785426",
+            link: "http://www.turismemiravet.cat/proposta/el-castell-de-miravet/",
+        },
+        {
+            Title: () => <>Centre Picasso</>,
+            Address: () => (
+                <>
+                    <p>Carrer de l'Hospital, s/n</p>
+                    <p>43596 Horta de Sant Joan</p>
+                </>
+            ),
+            Details: () => (
+                <>
+                    <p>
+                        Per preservar i promoure el llegat que el pintor malagueny va deixar en aquest municipi, el
+                        Centre Picasso exposa reproduccions facsímil de gairebé 200 obres realitzades en les dues
+                        estades que l'artista va fer a la vila i també d'altres quadres pintats a Barcelona o a París,
+                        però inspirats en el paisatge d'Horta. El Centre Picasso està situat a l'antic hospital, un
+                        edifici d'estil renaixentista que fou construït per acollir malalts i transeünts.
+                    </p>
+                </>
+            ),
+            link: "https://terresdelebre.travel/que-fer/cultura/museus-i-centres-dinterpretacio/centre-picasso-dhorta-de-sant-joan",
+        },
+        {
+            Title: () => <>Coves Meravelles</>,
+            Details: () => (
+                <>
+                    <p>
+                        Tot i que la seva existència ja era coneguda per la gent del poble, no va ser fins el 1968 quan
+                        el Centre d'Espeleologia de Gràcia les va explorar per primer cop. Les Meravelles són les més
+                        impressionants del conjunt de coves anomenades de l'Aumidiella. Amb una gran riquesa en
+                        formacions, mostren estalactites, estalagmites, banderes, gours, i fins i tot les anomenades
+                        excèntriques, que s'atreveixen a jugar amb la llei de la gravetat en créixer de manera
+                        inexplicable en totes les direccions possibles.
+                    </p>
+                </>
+            ),
+            link: "https://terresdelebre.travel/que-fer/espais-naturals/altres-espais-naturals/coves-meravelles",
+        },
+        {
+            Title: () => <>The wine cathedral</>,
+            Address: () => (
+                <>
+                    <p>Carrer del Pilonet, 8</p>
+                    <p>43594 El Pinell de Brai</p>
+                </>
+            ),
+            Details: () => (
+                <>
+                    <p>
+                        La Catedral del Vi del Pinell de Brai és l’edifici cooperativista d’estil modernista de
+                        principis del segle XX dissenyat per Cèsar Martinell, un deixeble directe d’Antoní Gaudí.
+                    </p>
+                </>
+            ),
+            link: "https://catedraldelvi.com/?v=e0ff4366bd30",
         },
     ],
 }
