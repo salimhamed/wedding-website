4b31568446406fe0f6b2b82092523e12121b669f
Lledo <lledoe@gmail.com>
2020-02-07 16:02:03 -0800

    català: travel

--- src/content/Travel/Travel.js
+++ src/content/Travel/Travel.js
@@ -1,14 +1,15 @@
 import React from "react"
 
-import { LANGUAGE } from "actions/constants"
+import {LANGUAGE} from "actions/constants"
 
 export default {
     [LANGUAGE.EN]: [
-        {  Title: () => <>Terres de l'Ebre</>,
-           Details: () => (
-               <>Les Terres de l'Ebre is located in the south-west of Catalonia, in the southern part of river Ebre.
-               It is a UNESCO Biosphere Reserve since 2013. We recommend to stay either in Tortosa or L'Ampolla.</>
-           ),
+        {
+            Title: () => <>Terres de l'Ebre</>,
+            Details: () => (
+                <>Les Terres de l'Ebre is located in the south-west of Catalonia, in the southern part of river Ebre.
+                    It is a UNESCO Biosphere Reserve since 2013. We recommend to stay either in Tortosa or L'Ampolla.</>
+            ),
             link: "https://terresdelebre.travel/en",
         },
         {
@@ -65,10 +66,10 @@ export default {
                     <p>
                         We recommend to stay either in Tortosa (Lledó's hometown) or L'Ampolla (if you want to stay by
                         the beach). The Welcome Pica-pica will be in L'Ampolla, and there will be buses from both
-                        Tortosa and L'Ampolla to Les Moles on the day of the weeding. In L'Ampolla we recommend Les
-                        Oliveres and Hotel Cap Roig; however, you will need car transportation (5 min drive) to the
+                        Tortosa and L'Ampolla to Les Moles on the day of the weeding. In L'Ampolla Les
+                        Oliveres and Hotel Cap Roig are good options; however, you will need car transportation (5 min drive) to the
                         Welcome Pica-pica.
-                        In Spain, we recommend to use booking.com instead of airbnb.com.
+                        In Spain, booking.com is usually better than airbnb.com.
                     </p>
                 </>
             ),
@@ -77,46 +78,75 @@ export default {
     ],
     [LANGUAGE.CAT]: [
         {
-            Type: () => <>Flight</>,
-            Title: () => <>BCN</>,
-            Address: () => (
-                <>
-                    <p>950 Mason Street</p>
-                    <p>San Francisco, CA 94108</p>
-                </>
+            Title: () => <>Terres de l'Ebre</>,
+            Details: () => (
+                <>Les Terres de l'Ebre es troba al sud-oest de Catalunya, al curs baix del riu Ebre.
+                    Al 2013, la UNESCO va declarar les Terres de l'Ebre com a espai natural i humà únic a tot el món,
+                    atorgant-li el reconeixement de Reserva Natural de la Biosfera.
+                    Us recomanem que us allotgeu a Tortosa o L'Ampolla.</>
             ),
+            link: "https://terresdelebre.travel",
+        },
+        {
+            Type: () => <>Vol</>,
+            Title: () => <>BCN</>,
+            Address: null,
             Phone: null,
             Details: () => (
                 <>
                     <p>
-                        This is the best place to fly into. You could go to
-                        Oakland or San Jose if it is easier for you to get
-                        there.
+                        L'aeroport de Barcelona és el que queda més proper.
                     </p>
                 </>
             ),
-            link: null,
+            link: "https://www.google.com/flights?lite=0#flt=SEA./m/01f62.2020-08-01*/m/01f62.SEA.2020-08-10;c:USD;e:1;sd:1;t:f",
         },
         {
-            Type: () => <>Lodging for Wedding</>,
-            Title: () => <>Fairmont San Francisco</>,
-            Address: () => (
-                <>
-                    <p>950 Mason Street</p>
-                    <p>San Francisco, CA 94108</p>
-                </>
-            ),
-            Phone: () => <>(415) 772-5000</>,
+            Type: () => <>Tren</>,
+            Title: () => <>Tortosa/L'Ampolla</>,
+            Address: null,
+            Phone: null,
             Details: () => (
                 <>
                     <p>
-                        This is where we’ll be having our rehearsal dinner and
-                        welcome drinks. And it’s not far from our ceremony,
-                        either.
+                        Renfe (Rogamos Empujen Nuestros Ferrocarriles Estropeados) us pot portar des de diferent parts
+                        de l'Estat Espanyol a Tortosa o L'Ampolla.
                     </p>
                 </>
             ),
-            link: "https://www.fairmont.com/san-francisco",
+            link: "http://www.renfe.com/CA/viajeros/index.html",
+        },
+        {
+            Type: () => <>Autobús</>,
+            Title: () => <>Tortosa/L'Ampolla</>,
+            Address: null,
+            Phone: null,
+            Details: () => (
+                <>
+                    <p>
+                        Hife és una altra bona opció, amd autobús directe a Tortosa i L'Ampolla.
+                    </p>
+                </>
+            ),
+            link: "http://hife.es",
+        },
+        {
+            Type: () => <>Allotjament</>,
+            Title: () => <>Tortosa o L' Ampolla</>,
+            Address: null,
+            Phone: null,
+            Details: () => (
+                <>
+                    <p>
+                        Us suggerim que us quedeu a Tortosa (d'on és la Lledó) of a L'Ampolla (si voleu estar al costat
+                        de la platja). El Pica-pica de Benvinguda serà a L'Ampolla, i hi haurà autobuss desde
+                        Tortosa i L'Ampolla a Les Moles el dia del casament. A L'Ampolla Les Oliveres i Hotel Cap Roig
+                        són bones opcions (però necessitaríeu cotxe per arribar al lloc del pica-pica).
+                        Podeu trobar altres opcions per booking.com.
+                    </p>
+                </>
+            ),
+            link: "https://www.booking.com/index.ca.html?label=gen173nr-1BCAEoggI46AdIM1gEaLQCiAEBmAEEuAEHyAEN2AEB6AEBiAIBqAIDuAKd2ffxBcACAQ;sid=9a89e727464401b3616fc4b7b66218c9;keep_landing=1&sb_price_type=total&",
         },
     ],
 }
