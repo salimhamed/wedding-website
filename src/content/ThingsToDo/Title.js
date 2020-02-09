dc9c6be6c8767ca18bfe53c133d336dc77fdffc5
Lledo <lledoe@gmail.com>
2020-02-08 16:03:59 -0800

    adding to title for things to do

--- src/content/ThingsToDo/Title.js
+++ src/content/ThingsToDo/Title.js
@@ -1,14 +1,14 @@
 import React from "react"
 
-import { LANGUAGE } from "actions/constants"
+import {LANGUAGE} from "actions/constants"
 
 export default {
     [LANGUAGE.EN]: {
         Heading: () => <>Things to Do</>,
         SubHeading: () => (
             <>
-                We’re so excited to share a few of our favorite places in the
-                area with you.
+                We’re so excited to share a few of our favorite places in the area with you. If you wish to explore
+                them, we recommend renting a car from Barcelona's airport.
             </>
         ),
         link: "https://terresdelebre.travel/en",
