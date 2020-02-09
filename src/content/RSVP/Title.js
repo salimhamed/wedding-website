6be0bd5cd753e1b14a7c8392ff2b81eda90764ae
Salim Hamed <salimhamed@gmail.com>
2020-02-08 18:16:34 -0800

    fix formatting

--- src/content/RSVP/Title.js
+++ src/content/RSVP/Title.js
@@ -5,28 +5,17 @@ import { LANGUAGE } from "actions/constants"
 export default {
     [LANGUAGE.EN]: {
         Heading: () => <>RSVP</>,
-        SubHeadingAuthenticated: () => (
-            <span>
-                Manage your RSVP below.
-            </span>
-        ),
+        SubHeadingAuthenticated: () => <span>Manage your RSVP below.</span>,
         SubHeading: () => (
             <span>
-                Sign in to manage your RSVP. Don't have an account? Click here to sign up!
+                Sign in to manage your RSVP. Don't have an account? Click here
+                to sign up!
             </span>
         ),
     },
     [LANGUAGE.CAT]: {
         Heading: () => <>RSVP</>,
-        SubHeadingAuthenticated: () => (
-            <span>
-                Manage your RSVP below.
-            </span>
-        ),
-        SubHeading: () => (
-            <span>
-                Sign up to manage your RSVP.
-            </span>
-        ),
+        SubHeadingAuthenticated: () => <span>Manage your RSVP below.</span>,
+        SubHeading: () => <span>Sign up to manage your RSVP.</span>,
     },
 }
