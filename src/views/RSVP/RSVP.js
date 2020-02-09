cc0d17cf322db43d85c62c1bf26219c8f9a53ddb
Salim Hamed <salimhamed@gmail.com>
2020-02-08 21:44:19 -0800

    Update image

--- src/views/RSVP/RSVP.js
+++ src/views/RSVP/RSVP.js
@@ -3,7 +3,7 @@ import React, { useContext } from "react"
 import { Store } from "store"
 import { title } from "content/RSVP"
 import { Header } from "components/Header"
-import headerImg from "photos/orcas.png"
+import headerImg from "photos/snow.png"
 
 import { RSVPForm } from "./Forms"
 
