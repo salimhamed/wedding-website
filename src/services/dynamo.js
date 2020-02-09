b60bf8909eddcea8111a5082ffbe41ae840ff577
Salim Hamed <salimhamed@gmail.com>
2020-02-08 18:55:51 -0800

    Add dynamodb client

---
+++ src/services/dynamo.js
@@ -0,0 +1,35 @@
+import Auth from "@aws-amplify/auth"
+import DynamoDB from "aws-sdk/clients/dynamodb"
+
+const getDocumentClient = () =>
+    Auth.currentCredentials().then(
+        credentials =>
+            new DynamoDB.DocumentClient({
+                region: process.env.REACT_APP_REGION,
+                credentials: Auth.essentialCredentials(credentials),
+            })
+    )
+
+export const putItem = async Item => {
+    if (!Item.UserId) {
+        return null
+    }
+
+    const params = {
+        TableName: process.env.REACT_APP_USER_TABLE,
+        Item,
+    }
+
+    const client = await getDocumentClient()
+    return await client.put(params).promise()
+}
+
+export const getItem = async Key => {
+    const params = {
+        TableName: process.env.REACT_APP_USER_TABLE,
+        Key,
+    }
+
+    const client = await getDocumentClient()
+    return await client.get(params).promise()
+}
