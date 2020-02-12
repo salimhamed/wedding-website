8c5a0b9da524f103e4259d9477cbfc43eb0e78ee
Lledo <lledoe@gmail.com>
2020-02-12 13:37:31 -0800

    add mosquito repellent info at Things to Do/Title

--- src/content/ThingsToDo/Title.js
+++ src/content/ThingsToDo/Title.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-import { LANGUAGE } from "actions/constants"
+import {LANGUAGE} from "actions/constants"
 
 export default {
     [LANGUAGE.EN]: {
@@ -9,7 +9,8 @@ export default {
             <>
                 We’re so excited to share a few of our favorite places in the
                 area with you. If you wish to explore them, we recommend renting
-                a car from Barcelona's airport.
+                a car from Barcelona's airport. At dusk, if you are planning
+                to be close to the water, we recommend to use mosquito repellent.
             </>
         ),
         link: "https://terresdelebre.travel/en",
@@ -17,7 +18,10 @@ export default {
     [LANGUAGE.CAT]: {
         Heading: () => <>Què fer</>,
         SubHeading: () => (
-            <>Aquí teniu algunes idees de què fer per les Terres de l'Ebre.</>
+            <>Aquí teniu algunes idees de què fer per les Terres de l'Ebre.
+                Al capvespre, si esteu a prop de l'aigua, recomanem utilitzar
+                repel·lent de mosquits.
+            </>
         ),
         link: "https://terresdelebre.travel",
     },
