4af23a3fba69765abe91a99900aa159b637801ea
Salim Hamed <salimhamed@gmail.com>
2021-01-24 19:19:50 -0800

    Love is patient

--- src/content/Home/Details.js
+++ src/content/Home/Details.js
@@ -7,14 +7,32 @@ const hashtag = "lledoisalim"
 export default {
     [LANGUAGE.EN]: {
         Title: () => <span>Salim and Lledó are getting married!</span>,
-        Date: () => <span>06/19/2021</span>,
+        Date: () => (
+            <div className="d-flex flex-column">
+                <div>
+                    <span style={{ textDecoration: "line-through" }}>
+                        06/19/2021
+                    </span>
+                </div>
+                <div>Love is patient...</div>
+            </div>
+        ),
         Location: () => <span>Terres de l&apos;Ebre, Catalonia</span>,
         locationLink: "https://goo.gl/maps/93vJLAEHGgZwUvET9",
         hashtag,
     },
     [LANGUAGE.CAT]: {
         Title: () => <span>El Salim i la Lledó es casen!</span>,
-        Date: () => <span>19/06/2021</span>,
+        Date: () => (
+            <div className="d-flex flex-column">
+                <div>
+                    <span style={{ textDecoration: "line-through" }}>
+                        19/06/2021
+                    </span>
+                </div>
+                <div>L'amor és pacient...</div>
+            </div>
+        ),
         Location: () => <span>Terres de l&apos;Ebre, Catalunya</span>,
         locationLink: "https://goo.gl/maps/93vJLAEHGgZwUvET9",
         hashtag,
